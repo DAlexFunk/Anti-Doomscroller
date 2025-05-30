abe10a8e511836f78f2a0fd3037280647178b433
Alex Funk <alexdagreat04@gmail.com>
2025-05-30 19:53:21 -0400

    Refactor: change from js styling to css injection
    also stopped service worker from terminating

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,7 @@
+const keepAlive = () => setInterval(chrome.runtime.getPlatformInfo, 20e3);
+chrome.runtime.onStartup.addListener(keepAlive);
+keepAlive();
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.url) {
--- scripts/content.js
+++ scripts/content.js
@@ -3,36 +3,42 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === "tabChange" && request.url.includes("shorts")) {
     // Get the short on the page
     // There are multiple video elements but only the short has an src so we filter for that
-    const short = Array.from(document.querySelectorAll("video")).filter((video) => video.src)[0];
+    const short = Array.from(document.querySelectorAll("video")).filter(
+      (video) => video.src
+    )[0];
 
     // Create diaog and style
-    const newP = document.createElement("dialog");
-    newP.textContent = "STOP SCROLLING";
-    newP.style.fontSize = "24px";
-    newP.style.height = "50vh";
-    newP.style.width = "50vw";
-    newP.style.background = "red";
-    newP.style.zIndex = 1000;
+    const popup = document.createElement("dialog");
+    popup.id = "popup";
+    popup.textContent = "STOP SCROLLING";
 
     // Create button, style, and add the click event
     const button = document.createElement("button");
+    button.id = "closePopup";
     button.onclick = () => {
-      newP.close();
-      if (short) short.play() // TODO: maybe remove this, it would be more annoying if you had to unpuase each time
-                              //       which could lead to less shorts time
+      popup.close();
+      console.log("test");
+      if (short) short.play();
     };
-    button.textContent = "Fine you can scroll";
-    newP.appendChild(button);
+    button.textContent = "X";
 
-    document.body.appendChild(newP);
-    
+    // Put the button in a random position
+    button.style.marginLeft = `${Math.random() * 100}%`;
+    button.style.marginTop = `${Math.random() * 20}%`;
+
+    buttonContainer = document.createElement("div");
+    buttonContainer.id = "buttonContainer";
+    buttonContainer.appendChild(button);
+
+    popup.appendChild(buttonContainer);
+    document.body.appendChild(popup);
+
     // Show the dialog
-    newP.showModal();
+    popup.showModal();
 
     // Pause the short (if the short takes too long to load it may not be here so we check for it)
     if (short) {
       short.pause();
     }
-
   }
 });
