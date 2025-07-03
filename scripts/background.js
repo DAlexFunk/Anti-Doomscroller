c01d1c0c00cc964a6373e56fdbd29348937eb9a7
Alex Funk <alexdagreat04@gmail.com>
2025-07-03 14:57:24 -0400

    Fix: actually fix inactive service worker

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,8 @@
 const keepAlive = () => setInterval(chrome.runtime.getPlatformInfo, 20e3);
 chrome.runtime.onStartup.addListener(keepAlive);
+keepAlive();
 
 chrome.runtime.onInstalled.addListener(() => {
-  keepAlive();
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.url) {
       chrome.tabs.sendMessage(tabId, {
